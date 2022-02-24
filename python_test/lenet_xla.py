fix HandleConvolution for BackpropFilter (#79)

python_test/lenet_xla.py
@@ -33,8 +33,13 @@ def load_data():
 
 (x_train, y_train), (x_test, y_test) = load_data()
 
-x_train = x_train[0:5,:,:,:]
-y_train = y_train[0:5,:]
+BATCH_SIZE = 8
+TRAIN_SIZE = BATCH_SIZE
+EPOCHS = 2
+DUMP_DIR = "npu"
+
+x_train = x_train[0:TRAIN_SIZE, :, :, :]
+y_train = y_train[0:TRAIN_SIZE, :]
 
 x_test = x_test[0:1,:,:,:]
 y_test = y_test[0:1,:]
@@ -60,8 +65,8 @@ def compile_model(model):
                 metrics=['accuracy'])
   return model
 
-def train_model(model, x_train, y_train, x_test, y_test, epochs=1):
-  model.fit(x_train, y_train, batch_size=5, epochs=epochs, validation_data=(x_test, y_test), shuffle=True)
+def train_model(model, x_train, y_train, x_test, y_test, epochs=1, batch_size=1):
+  model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(x_test, y_test), shuffle=False)
 
 def warmup(model, x_train, y_train, x_test, y_test):
   # Warm up the JIT, we do not wish to measure the compilation time.
@@ -77,8 +82,6 @@ else:
   model = generate_model()
 
 model = compile_model(model)
-
-DUMP_DIR = "npu"
 
 def dump_tensors(tensors, prefix, tensors_name=None):
   if not os.path.isdir(DUMP_DIR): os.makedirs(DUMP_DIR)
@@ -108,7 +111,7 @@ def get_weight_grad(model, inputs, outputs):
   return grad
 
 #warmup(model, x_train, y_train, x_test, y_test)
-train_model(model, x_train, y_train, x_test, y_test, epochs=4)
+train_model(model, x_train, y_train, x_test, y_test, epochs=EPOCHS, batch_size=BATCH_SIZE)
 model.summary()
 
 weight_grads = get_weight_grad(model, x_train, y_train)
@@ -127,5 +130,7 @@ model.save_weights(DUMP_DIR + "/after/" + MODEL_DATA_FILE)
 # functor = tf.keras.backend.function([model.input], outputs)
 # layer_outs = functor([x_train])
 # dump_tensors(layer_outs, "output", outputs)
+
+print(model.predict(x_test))
 
 print("RRR : job finish.")